d0e64336ed7937c42bf193097e478ecad57d8506
Teeradaj Rachrak <r.teeradaj@gmail.com>
2023-07-14 15:19:30 +0900

    Initial commit
    Initial commit

---
+++ src/main/java/jaist/realearn/consistentargumentsfinder/ConsistentArgumentsFinderApplication.java
@@ -0,0 +1,13 @@
+package jaist.realearn.consistentargumentsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsistentArgumentsFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsistentArgumentsFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jaist/realearn/consistentargumentsfinder/ConsistentArgumentsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package jaist.realearn.consistentargumentsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistentArgumentsFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
