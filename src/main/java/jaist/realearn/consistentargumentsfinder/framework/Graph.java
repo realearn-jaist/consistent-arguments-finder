5563ca28ae2d57793cd3baab483582aac63a416d
s-ham-shimi <139534786+s-ham-shimi@users.noreply.github.com>
2023-08-07 14:09:41 +0900

    minor change

--- src/main/java/jaist/realearn/consistentargumentsfinder/framework/Graph.java
+++ src/main/java/jaist/realearn/consistentargumentsfinder/framework/Graph.java
@@ -1,6 +1,6 @@
 package jaist.realearn.consistentargumentsfinder.framework;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Graph {
         List<Node> nodes;
--- src/main/java/jaist/realearn/consistentargumentsfinder/framework/Main.java
+++ src/main/java/jaist/realearn/consistentargumentsfinder/framework/Main.java
@@ -3,10 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.awt.Dimension;
-import java.awt.geom.Point2D;
-import javax.swing.JFrame;
-import org.jgrapht.Graph;
+
+
 
 
 
@@ -34,17 +32,8 @@ public class Main {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        double d = 100.0;
-        double i = 1;
-        for(Graph n : g) {
-            g.getName().setPoint(d*i,d);
-            i++;
-        }
 
-        JFrame frame = new JFrame("Abstract Argumentation Figure");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+
         }
 
     }
